a1a7eab6f61d49b112c44a95edf54e18043f1038
Pranav M <pranavm265@gmail.com>
2024-06-23 03:15:07 +0530

    splash 90%

--- truthtrace-frontend/truthtrace/lib/screens/splash.dart
+++ truthtrace-frontend/truthtrace/lib/screens/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:page_transition/page_transition.dart';
 import 'package:truthtrace/screens/chat.dart';
 
@@ -10,30 +11,73 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
+  bool _serverConnected = false; // Flag to track server connection
+
+  @override
+  void initState() {
+    super.initState();
+    checkServerConnection();
+  }
+
+  Future<void> checkServerConnection() async {
+    try {
+      final response = await http.get(Uri.parse('https://truthtracebackend.onrender.com/'));
+
+      if (response.statusCode == 405) {
+        // Server is reachable
+        setState(() {
+          _serverConnected = true;
+        });
+        navigateToChatScreen();
+      } else {
+        // Server returned an error status code
+        showServerNotConnectedDialog();
+      }
+    } catch (e) {
+      // Error connecting to server
+      showServerNotConnectedDialog();
+    }
+  }
+
+  void showServerNotConnectedDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text("Server Not Connected"),
+        content: const Text("Please check your internet connection or try again later."),
+        actions: <Widget>[
+          TextButton(
+            child: const Text("OK"),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void navigateToChatScreen() {
+    if (_serverConnected) {
+      Navigator.pushReplacement(
+        context,
+        PageTransition(type: PageTransitionType.bottomToTop, child:const Chat()),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
+            Text(
               "truth-Trace\nFind-the-Truth",
-              style: TextStyle(fontFamily: 'PlayWrite',
-              fontSize: 32),
+              style: TextStyle(fontFamily: 'PlayWrite', fontSize: 32),
               textAlign: TextAlign.center,
             ),
-            const SizedBox(
-              height: 50,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context, PageTransition(child: Chat(), type: PageTransitionType.bottomToTop),
-                );
-              },
-              child: const Text('Hehe'),
-            )
           ],
         ),
       ),
