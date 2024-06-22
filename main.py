4f08c022af3fee430585cfccc238e4f41a4d1c8d
drogon <abhiramajithr@gmail.com>
2024-06-23 02:22:14 +0530

    finished-furrnished-output

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
-from scrap import scrap_body
-from langchain_utils import link_generation
-from scrap import scrap_body
+from bs4 import BeautifulSoup
+from langchain_core.tools import Tool
+from langchain_google_community import GoogleSearchAPIWrapper
 import os
+import requests
 
+#loading enviornment variables
 dotenv_path='.env'
 load_dotenv(dotenv_path)
 
+#loading model
 llm = chat = ChatGroq(
-    temperature=0,
+    temperature=0.6,
     model="llama3-70b-8192",
 )
 
 prompt_template=PromptTemplate.from_template("this is a user's text:{user_input}\nthis is data from websites related to user's text:{headlines}\n.Compare the user's text with data from the provided websites and give an approximate percentage of how accurate the data is matching with user's text and at the end only show the percentage accuracy")
-
 
 def compare_with_llm(user_input,content):
     chain = prompt_template | llm
@@ -24,24 +26,45 @@ def compare_with_llm(user_input,content):
 
 def body_text(text):
     links = link_generation(text)
-    body = ''
-    if links is not None:
-        # for link in links:
-        #     body = scrap_body(link)
-        print("hi")
-    else: 
-        print("hello")
+    for link in links:
+        body = scrap_body(link)
     return body
+
+search = GoogleSearchAPIWrapper()
+
+def top_5_result(text):
+    return search.results(text,5)
+
+tool = Tool(
+    name="google_search",
+    description="Search Google for recent results.",
+    func=top_5_result,
+)
+
+def extract_links(results):
+    links = []
+    for item in results:
+        links.append(item['link'])
+    return links
+
+def link_generation(user_input):
+    results=top_5_result(user_input)
+    links=extract_links(results)
+    return links
+
+def scrap_body(url):
+    if url is not None:
+        session = requests.session()
+        siteResponse = session.get(url)
+        if siteResponse.status_code == 200:
+            siteHtml = BeautifulSoup(siteResponse.content, 'html.parser')
+            if siteHtml.body is not None:
+                return siteHtml.body.text
 
 def final_result(text):
     content = body_text(text)
     result = compare_with_llm(text,content)
-    return content
+    return f"output:{result.content}\nsource:{link_generation(text)}"
 
-def testing():
-    prompt="Does fentanyl cause headache"
-    content=scrap_body('https://www.drugs.com/')
-    result=compare_with_llm(prompt,content)
-    return result
-print(testing())
+print(final_result("xanax side effects"))
 
